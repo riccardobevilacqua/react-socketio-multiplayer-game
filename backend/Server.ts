b41fb6c625f374566a1e875de9f1ec76cfe0b498
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-02 23:21:40 +0100

    Begin

---
+++ backend/Server.ts
@@ -0,0 +1,18 @@
+import { Express, Request, Response } from 'express';
+
+export class Server {
+
+  private app: Express;
+
+  constructor(app: Express) {
+    this.app = app;
+
+    this.app.get('/api', (_req: Request, res: Response): void => {
+      res.send('You have reached the API!');
+    })
+  }
+
+  public start(port: number): void {
+    this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
+  }
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+import { Server } from "./backend/Server";
+import express from 'express';
+
+const app = express();
+
+const port = 8080;
+
+const server = new Server(app);
+server.start(port);
