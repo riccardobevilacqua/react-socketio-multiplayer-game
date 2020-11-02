9117e465ad422a802f0a226cb209376233a5ecd0
Riccardo Bevilacqua <bevilacqua.riccardo@gmail.com>
2020-11-02 23:33:34 +0100

    Served client via server

--- backend/Server.ts
+++ backend/Server.ts
@@ -1,15 +1,22 @@
 import { Express, Request, Response } from 'express';
+import express from 'express';
+import * as path from 'path';
 
 export class Server {
-
   private app: Express;
 
   constructor(app: Express) {
     this.app = app;
 
-    this.app.get('/api', (_req: Request, res: Response): void => {
+    this.app.use(express.static(path.resolve('./') + '/build/frontend'));
+
+    this.app.get('/api', (req: Request, res: Response): void => {
       res.send('You have reached the API!');
-    })
+    });
+
+    this.app.get('*', (req: Request, res: Response): void => {
+      res.sendFile(path.resolve('./') + '/build/frontend/index.html');
+    });
   }
 
   public start(port: number): void {
